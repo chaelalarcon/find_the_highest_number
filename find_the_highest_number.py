05158a8865c081a10b873b522e8eedf73cbd8f51
unknown <johnmichaelalarcon2006@gmail.com>
2024-10-16 08:53:09 +0700

    pseudocode

---
+++ find_the_highest_number.py
@@ -0,0 +1,16 @@
+# define 5 variables
+# compare var1 to var2, var3, var4 and var5
+# if var1 > var2 and var1 > var3 and var1 > var4 and var1 > var5:
+   # print var1 as the highest number
+# compare var2 to var1, var3, var4 and var5
+# elif var2 > var1 and var2 > var3 and var2 > var4 and var2 > var5:
+   # print var2 as the highest number
+# compare var3 to var1, var2, var4 and var5
+# elif var3 > var1 and var3 > var2 and var3 > var4 and var3 > var5:
+   # print var3 as the highest number
+# compare var4 to var1, var2, var3 and var5
+# elif var4 > var1 and var4 > var2 and var4 > var3 and var4 > var5:
+   # print var4 as the highest number
+
+# if not:
+   # print var5 as the highest number 
