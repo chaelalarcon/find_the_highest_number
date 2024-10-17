7ac99b28206adabac7c99598ec0915598d81ca7b
unknown <johnmichaelalarcon2006@gmail.com>
2024-10-17 22:43:27 +0700

    pseudocode (I use the methods approach and define function

--- find_the_highest_number.py
+++ find_the_highest_number.py
@@ -1,35 +1,23 @@
 # define 5 variables
+# define function
 # compare var1 to var2, var3, var4 and var5
-# if var1 > var2 and var1 > var3 and var1 > var4 and var1 > var5:
-   # print var1 as the highest number
+   # if var1 >= var2 and var1 >= var3 and var1 >= var4 and var1 >= var5:
+      # print var1 as the highest number 
+      # return var1
 # compare var2 to var1, var3, var4 and var5
-# elif var2 > var1 and var2 > var3 and var2 > var4 and var2 > var5:
-   # print var2 as the highest number
+   # elif var2 >= var1 and var2 >= var3 and var2 >= var4 and var2 >= var5:
+      # print var2 as the highest number
+      # return var2
 # compare var3 to var1, var2, var4 and var5
-# elif var3 > var1 and var3 > var2 and var3 > var4 and var3 > var5:
-   # print var3 as the highest number
+   # elif var3 >= var1 and var3 >= var2 and var3 >= var4 and var3 >= var5:
+      # print var3 as the highest number
+      # return var3
 # compare var4 to var1, var2, var3 and var5
-# elif var4 > var1 and var4 > var2 and var4 > var3 and var4 > var5:
+   # elif var4 >= var1 and var4 >= var2 and var4 >= var3 and var4 >= var5:
    # print var4 as the highest number
+      # return var4
 
 # if not:
    # print var5 as the highest number
 
-
-first = int(input("Enter 1st number: "))
-second = int(input("Enter 2nd number: "))
-third = int(input("Enter 3rd number: "))
-fourth = int(input("Enter 4th number: "))
-fifth = int(input("Enter 5th number: "))
-
-if first > second and first > third and first > fourth and first > fifth:
-    print("First number is the highest number")
-elif second > first and second > third and second > fourth and second > fifth:
-    print("Second number is the highest number")
-elif third > first and third > second and third > fourth and third > fifth:
-    print("Third number is the highest number")
-elif fourth > first and fourth > second and fourth > third and fourth > fifth:
-    print("Fourth number is the highest number")
-
-else:
-    print("Fifth number is the highest number")
+# return function
